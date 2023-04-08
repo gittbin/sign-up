a7763588715377e2802907d85cd8c759092b9157
gittbin <tranledung2907@gmail.com>
2023-04-08 12:47:24 +0700

    upload_sign_up

---
+++ index.js
@@ -0,0 +1,88 @@
+var $=document.querySelector.bind(document)
+var $$=document.querySelectorAll.bind(document)
+var label=$$('label')
+var input=$$('input')
+var error=$$('.error')
+var a=$$('a')
+var button=$('.button')
+var getrePassword=$('.getrePassword')
+var getPassword=$('.getPassword')
+
+function getParent(element,class_of_parent){
+    let check=true
+    var parent=element
+    
+    while(check){
+        parent=parent.parentElement
+         var class_of_parents=parent.classList   
+        for(var i=0;i<class_of_parents.length;i++){
+    if(class_of_parents[i]==class_of_parent){
+        check=false
+    } }
+    }
+   return parent
+    
+}
+input.forEach(function(element,index){
+    if(element.value){
+        error[index].style.display="none"
+        label[index].style.transform="translateY(0px)"
+        label[index].style.color="rgb(0, 255, 242)"
+      element.style.borderBottom="4px solid rgb(0, 255, 242)"
+    }
+  element.onclick=function(){
+error[index].style.display="none"
+  label[index].style.transform="translateY(0px)"
+  label[index].style.color="rgb(0, 255, 242)"
+element.style.borderBottom="4px solid rgb(0, 255, 242)"
+}
+  element.onblur=function(){
+    if(element.value){}
+    else{
+    label[index].style.transform="translateY(25px)"
+    label[index].style.color="rgb(247, 247, 247,0.5)"
+    element.style.borderBottom="2px solid rgb(247, 247, 247)"}
+}});
+
+button.onclick=function(e){
+e.preventDefault();
+    if(getrePassword.querySelector('input').value!=getPassword.querySelector('input').value&&getrePassword.querySelector('input').value){
+let error=getrePassword.querySelector('.error')
+    error.innerHTML="mật khẩu nhập lại không đúng"
+    error.style.display="block"
+  }
+  else{
+    let error=getrePassword.querySelector('.error')
+    error.style.display="none"
+    error.innerHTML="vui lòng nhập lại mật khẩuu"
+
+  }
+
+    input.forEach(function(element,index){
+    if(element.value){    
+
+    }
+    else{
+    error[index].style.display="block"
+    }
+  })
+  var checkerror=true
+  input.forEach(function(element,index){
+
+    if(error[index].style.display=="none"){
+
+    }
+else{
+  checkerror=false
+}
+
+  })
+  console.log(checkerror)
+  if(checkerror){
+    input.forEach(function(element,index){
+    if(input[index].name!=""){
+    localStorage.setItem(input[index].name,input[index].value)
+    }
+  })
+}
+}
